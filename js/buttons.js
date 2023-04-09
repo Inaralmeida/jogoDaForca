695dac2c3abb9ed263f3750e8260eb8ffee38a25
inaralmeida <inara.ralmeida@gmail.com>
2023-04-09 01:00:31 -0300

    feat: add tips by buttons

--- js/buttons.js
+++ js/buttons.js
@@ -2,27 +2,34 @@ import { printLettersCorrect, updatedLettersCorrect } from "./letters.js";
 import { createElement } from "./utils.js";
 
 export const createButtonsLetters = (id, letras) => {
-    letras.forEach((letra) => {
-      const btn = createElement("button", letra, "btn-letra", "", letra);
-      btn.onclick = () => handleClickLetter(btn);
-      id.appendChild(btn);
-    });
-  };
-  
-  export const handleClickLetter = (button) => {
-    const letter = button.value;
-    const word = window.localStorage.getItem("word").toUpperCase();
-    
-    if (word.indexOf(letter) > -1) {
-      
-      printLettersCorrect(updatedLettersCorrect(letter, word))
-      addStyledButton("certa", button);
-      
-    } else {
-      addStyledButton("errada", button);
-    }
-  };
-  
-  const addStyledButton = (type, element) => {
-    element.classList.add(type);
-  };
+  letras.forEach((letra) => {
+    const btn = createElement("button", letra, "btn-letra", "", letra);
+    btn.onclick = () => handleClickLetter(btn);
+    id.appendChild(btn);
+  });
+};
+
+const handleClickLetter = (button) => {
+  const letter = button.value;
+  const word = window.localStorage.getItem("word").toUpperCase();
+
+  if (word.indexOf(letter) > -1) {
+    printLettersCorrect(updatedLettersCorrect(letter, word));
+    addStyledButton("certa", button);
+  } else {
+    addStyledButton("errada", button);
+  }
+};
+
+const addStyledButton = (type, element) => {
+  element.classList.add(type);
+};
+
+export const handleClickTip = (e, id)=>{
+    e.preventDefault()
+    document.querySelectorAll('.dica').forEach((tip, i )=>{
+        if(i == id){
+            tip.style.display = 'flex'
+        }
+    })
+}
--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
+import { createButtonsLetters, handleClickTip } from "./buttons.js";
 import { letterData, wordData} from "./data.js";
-import { createButtonsLetters} from "./letters.js";
 import { selectWord } from "./word.js";
 
 
 const teclado = document.querySelector('#teclado')
+const btnTips = document.querySelectorAll('.btnDica')
 
 createButtonsLetters(teclado, letterData)
-
 selectWord(wordData)
+
+btnTips.forEach((btn, i )=>{
+    // console.log()
+    btn.addEventListener('click', (e)=>handleClickTip(e, i))
+})
