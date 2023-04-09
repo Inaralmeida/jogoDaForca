5e85e2078ef20278db928eaf965bb0897adfe58f
inaralmeida <inara.ralmeida@gmail.com>
2023-04-08 21:15:43 -0300

    feat: add mock data

---
+++ js/data.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    palavra: "Banana",
+    dicas: {
+      1: "Fruta",
+      2: "Amarela",
+      3: "Tem pintinha pretas",
+    },
+  },
+  {
+    palavra: "Faculdade",
+    dicas: {
+      1: "Muitos cursos",
+      2: "Formação",
+      3: "Academico",
+    },
+  },
+  {
+    palavra: "Cama",
+    dicas: {
+      1: "Dormir",
+      2: "Quarto",
+      3: "Colchão",
+    },
+  },
+];
