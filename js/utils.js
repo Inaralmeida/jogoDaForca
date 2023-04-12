f4e6f55a7b6f6004d2cd55695a61f2c0239a19ee
inaralmeida <inara.ralmeida@gmail.com>
2023-04-12 01:39:36 -0300

    feat: object utils

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,47 @@
-export const createElement = (tag, content, className, id = '', value ='') => {
+
+
+const createElement = (tag, content, className, id = '', value ='') => {
     const element = document.createElement(tag);
     element.textContent= content
     element.id = id
     element.value= value
     element.classList.add(className)
     return element
+}
+
+const addEventsButons = (listButons, action) =>{
+    listButons.forEach((btn, i )=>{
+      btn.addEventListener('click', (e)=>action(e, i))
+  })
+  }
+
+const setLocalStorage = (key, value)=>{
+    window.localStorage.setItem(key, JSON.stringify(value))
+}
+
+const getLocalStorage = (key)=>{
+    const data = JSON.parse(window.localStorage.getItem(key))
+    return data
+}
+
+const removeLocalStorage = (key)=>{
+    window.localStorage.removeItem(key)
+}
+
+const resetLocalStorage = (key, value)=>{
+    removeLocalStorage(key)
+    setLocalStorage(key, value)
+}
+
+const setDisplayElement = (element, display) =>{
+    element.style.display = display
+}
+
+export default {
+    createElement,
+    addEventsButons,
+    getLocalStorage,
+    setLocalStorage,
+    resetLocalStorage,
+    setDisplayElement
 }
