0c52661de1e895b15401327ff1786618524c1e46
inaralmeida <inara.ralmeida@gmail.com>
2023-04-12 01:40:42 -0300

    feat: start game

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
 import { addEventsButons, createButtonsLetters} from "./buttons.js";
 import { letterData, wordData} from "./data.js";
+import { createdArrLettersCorrect, createdSpaceLetters } from "./letters.js";
+import { createdTips } from "./tips.js";
+import $ from "./utils.js";
 import { selectWord } from "./word.js";
 
 
 const teclado = document.querySelector('#teclado')
 const btnTips = document.querySelectorAll('.btnDica')
 
-createButtonsLetters(teclado, letterData)
-selectWord(wordData)
+const startGame = ()=>{
+  createButtonsLetters(teclado, letterData)
 
-addEventsButons(btnTips)
+  const wordSelected = selectWord(wordData)
+  $.resetLocalStorage('letters', createdArrLettersCorrect(wordSelected.palavra.split('')))
+  $.resetLocalStorage('lettersWrong', [])
+
+  createdTips(wordSelected.dicas)
+  createdSpaceLetters(wordSelected.palavra)
+  addEventsButons(btnTips)
+}
+
+startGame()
+
+
+
+
