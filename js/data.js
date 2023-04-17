b878938d1437fd6b4f4d9676dd1b339a9e0959f8
inaralmeida <inara.ralmeida@gmail.com>
2023-04-16 23:35:38 -0300

    fix: add new word

--- js/data.js
+++ js/data.js
@@ -4,15 +4,48 @@
 export const wordData = [
   {
     palavra: "Banana",
-    dicas: ["Fruta", "Amarela", "Tem pintinha pretas"],
+    dicas: ["Amarela", "Tem pintinha preta", "Usa pijama"],
+    categoria: "Fruta",
   },
   {
-    palavra: "Faculdade",
-    dicas: ["Muitos cursos", "Formação", "Academico"],
+    palavra: "Programador",
+    dicas: ["Faz programa", "Trabalha duro", "viciad@ em café"],
+    categoria: "Profissão",
   },
   {
     palavra: "Cama",
     dicas: ["Dormir", "Quarto", "Colchão"],
+    categoria: "Objeto",
+  },
+  {
+    palavra: "Anel",
+    dicas: ["Circular", "Dedo", "brilha"],
+    categoria: "Objeto",
+  },
+  {
+    palavra: "Carro",
+    dicas: ["Anda", "Trasmporta", "Toca música"],
+    categoria: "Objeto",
+  },
+  {
+    palavra: "Cadeira",
+    dicas: ["Descanso", "Tem encosto", "Pode girar"],
+    categoria: "Objeto",
+  },
+  {
+    palavra: "Mesa",
+    dicas: ["Apoio", "Varios formatos", "Pode ser de jantar"],
+    categoria: "Objeto",
+  },
+  {
+    palavra: "Fusca",
+    dicas: ["Vintage", "Herb", "Azul é perigoso"],
+    categoria: "Carro",
+  },
+  {
+    palavra: "Televisao",
+    dicas: ["Passa-tempo", "Caro", "Eletronico"],
+    categoria: "Objeto",
   },
 ];
 
